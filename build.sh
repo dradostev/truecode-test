c144b76eb42a2cbe91efc5d2e8b3e74b933e51af
Daniil Radostev <d.a.radostev@gmail.com>
2025-02-10 20:14:56 +0300

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
-﻿docker build -t svc-auth:latest -f src/TrueCode.Services.Auth/Dockerfile .
+﻿eval $(minikube docker-env)
+
+docker build -t svc-auth:latest -f src/TrueCode.Services.Auth/Dockerfile .
 docker build -t svc-currencies:latest -f src/TrueCode.Services.Currencies/Dockerfile .
 docker build -t api-gw:latest -f src/TrueCode.API.Web/Dockerfile .
 docker build -t svc-fetcher:latest -f src/TrueCode.Services.Fetcher/Dockerfile .
 docker build -t job-migrator:latest -f src/TrueCode.Services.Migrator/Dockerfile .
+
+minikube addons enable ingress
+
+minikube kubectl -- apply -f k8s
